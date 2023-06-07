ccde2506ee3d300ff680f35e5fffbeff88bc52d4
bergmazz <ebergmann@uarts.edu>
2023-06-07 19:49:04 -0400

    home page connected

---
+++ app/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+
+from .config import Config
+# from .models import db
+from .routes import home
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+app.register_blueprint(home.bp)
